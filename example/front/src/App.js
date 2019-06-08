a01f12fd792eced68519b177907466e2f7d078d6
浪浪浪 <675483520@qq.com>
2019-06-09 02:29:47 +0800

    init win dev env & add demo style

--- example/front/src/App.js
+++ example/front/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QRcode from 'qrcode.react'
+import styles from './styles.module.css'
 import './App.css';
 
+const EmptyServerUrl = () => (
+  <section style={{ marginTop: '2rem' }}>输入测试url后将生成二维码，使用wechat扫一扫测试</section>
+)
+
+const QRcodeRender = ({ value }) => (
+  <div style={{ textAlign: 'center', marginTop: '2rem'}}>
+    <QRcode value={value} />
+    <p>使用微信扫一扫~</p>
+  </div>
+)
+
 function App() {
+  const [url, setUrl] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [serverUrl, setServerUrl] = useState(null)
+  const handleSubmit = async e => {
+    e && e.preventDefault()
+    try {
+      setLoading(true)
+      const serverData = await fetch(`/api/geturl?url=${url}`)
+      const { data, type, msg } = await serverData.json()
+      setLoading(false)
+      if (!type) alert(msg)
+      setServerUrl(data)
+    } catch (error) {
+      alert('服务器在发呆😐~')
+      setLoading(false)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>微信跳出浏览器Demo</h1>
       </header>
+      <section className={styles.container}>
+        <form onSubmit={handleSubmit} className={styles.wrapper}>
+          <input onChange={e => setUrl(e.target.value)} className={styles.input} />
+          <button type="submit" className={styles.btn}>{ loading ? 'loading...' : 'GO!'}</button>
+        </form>
+      </section>
+      <footer>
+        {
+          serverUrl ? <QRcodeRender value={serverUrl} /> : <EmptyServerUrl />
+        }
+      </footer>
     </div>
   );
 }
--- example/front/src/setupProxy.js
+++ example/front/src/setupProxy.js
@@ -1,5 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(proxy('/api', { target: 'http://localhost:3005/' }));
+  app.use(proxy('/api', { 
+    target: 'http://localhost:3005/',
+    pathRewrite: {
+      '/api': '/'
+    }
+  }));
 };
