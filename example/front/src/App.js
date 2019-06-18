a2a2c6decf7741e8045a3e0eadf6f6a22f9c7897
3lang <675483520@qq.com>
2019-06-18 15:00:13 +0800

    update demo ui

--- example/front/src/App.js
+++ example/front/src/App.js
@@ -3,28 +3,55 @@ import QRcode from 'qrcode.react'
 import styles from './styles.module.css'
 import './App.css';
 
-const EmptyServerUrl = () => (
-  <section style={{ marginTop: '2rem' }}>输入测试url后将生成二维码，使用wechat扫一扫测试</section>
+const DemoApkSourceUrl = `${window.location.protocol}//${window.location.host}/sources/demo.apk`
+const DemoUrl = 'https://github.com'
+
+const GithubLab = () => (
+  <a className={styles.github} href="https://github.com/EthanOrange/wechat-redirect"><strong className={styles.bgLiner}>Github</strong></a>
 )
+
+const PageHeader = () => (
+  <header className="App-header">
+    <h1 style={{ fontSize: '3rem' }}>Wechat Redirect Demo</h1>
+    <p style={{ color: '#aaa', marginBottom: '2rem' }}>输入测试url后将生成二维码，使用wechat扫一扫测试</p>
+  </header>
+)
+
+const ExampleItems = ({ onClick }) => {
+  return (
+    <section className={styles.itemContainer}>
+      特性预览：
+      <div onClick={() => onClick(DemoApkSourceUrl)} className={styles.item}>
+        <strong className={styles.bgLiner}>APK下载</strong>
+      </div>
+      <div onClick={() => onClick(DemoUrl)} className={styles.item}>
+        <strong className={styles.bgLiner}>URL跳出</strong>
+      </div>
+    </section>
+  )
+}
 
 const QRcodeRender = ({ value }) => {
   const shortUlr = `${window.location.protocol}//${window.location.host}/jump/${value}`
   return (
-  <div style={{ textAlign: 'center', marginTop: '2rem'}}>
-    <QRcode value={shortUlr} />
-    <p>使用微信扫一扫~(<a href={shortUlr}>{shortUlr}</a>)</p>
-  </div>
-)}
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <div style={{ background: '#fff', padding: '.35rem .35rem .2rem', display: 'inline-block' }}>
+        <QRcode bgColor="#fff" fgColor="#000" value={shortUlr} />
+      </div>
+      <p><strong className={styles.bgLiner}>使用微信扫一扫或直接用微信浏览器打开下面网址</strong></p>
+      <span style={{ background: '#fff', padding: '10px 20px', borderRadius: 20 }}><strong className={styles.bgLiner}><a href={shortUlr}>{shortUlr}</a></strong></span>
+    </div>
+  )
+}
 
 function App() {
-  const [url, setUrl] = useState('')
+  const [url, setUrl] = useState()
   const [loading, setLoading] = useState(false)
   const [serverUrl, setServerUrl] = useState(null)
-  const handleSubmit = async e => {
-    e && e.preventDefault()
+  const getUrlService = async urlStr => {
     try {
       setLoading(true)
-      const serverData = await fetch(`/api/geturl?url=${url}`)
+      const serverData = await fetch(`/api/geturl?url=${urlStr}`)
       const { data, type, msg } = await serverData.json()
       setLoading(false)
       if (!type) return alert(msg)
@@ -34,21 +61,26 @@ function App() {
       setLoading(false)
     }
   }
+  const handleSubmit = async e => {
+    e && e.preventDefault()
+    getUrlService(url)
+  }
+  const handleExample = async urlStr => {
+    getUrlService(urlStr)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>微信跳出浏览器Demo</h1>
-      </header>
+      <GithubLab />
+      <PageHeader />
       <section className={styles.container}>
         <form onSubmit={handleSubmit} className={styles.wrapper}>
-          <input onChange={e => setUrl(e.target.value)} className={styles.input} />
-          <button type="submit" className={styles.btn}>{ loading ? 'loading...' : 'GO!'}</button>
+          <input onChange={e => setUrl(e.target.value)} className={styles.input} placeholder="eg:https://github.com" />
+          <button type="submit" className={styles.btn}>{loading ? 'loading...' : 'Submit'}</button>
+          <ExampleItems onClick={handleExample} />
         </form>
       </section>
-      <footer>
-        {
-          serverUrl ? <QRcodeRender value={serverUrl} /> : <EmptyServerUrl />
-        }
+      <footer style={{ width: '100%' }}>
+        {serverUrl && <QRcodeRender value={serverUrl} />}
       </footer>
     </div>
   );
